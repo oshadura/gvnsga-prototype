a93f6bd547a018cbf0754db352d2cf70605095bd
oshadura <oksana.shadura@cern.ch>
2016-04-25 11:12:39 -0400

    Cleaning up unused files (due to abandoned concept of complex vector containers)

---
+++ libga/inc/GAGoogleTest.h
@@ -0,0 +1,19 @@
+#ifndef GAGOOGLE_TEST_H
+#define GAGOOGLE_TEST_H
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using namespace ::testing;
+
+class GAGoogleTest
+{
+public:
+	
+	/*
+	GAGoogleTest(arguments);
+	~GAGoogleTest();
+	*/
+	
+	/* data */
+};
