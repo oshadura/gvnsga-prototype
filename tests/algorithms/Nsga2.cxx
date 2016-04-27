3e85710aced7c653f5b65ef53d869d5292dcd914
oshadura <oksana.shadura@cern.ch>
2016-04-27 08:05:46 -0400

    Adding possibility of google testing

---
+++ tests/algorithms/Nsga2.cxx
@@ -0,0 +1,12 @@
+#include "GATest.h"
+#include "algorithms/AlgorithmNSGA.h"
+//#include "problems/DTLZ.h"
+
+
+class Nsga2 : public GATest {
+public:
+};
+
+TEST_F(Nsga2, TrainingSolvingProblem) {
+	;
+}
---
+++ tests/inc/GATest.h
@@ -0,0 +1,14 @@
+#ifndef GATEST_H
+#define GATEST_H
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+using namespace ::testing;
+
+class GATest : public ::testing::Test {
+public:
+
+};
+
+#endif 
