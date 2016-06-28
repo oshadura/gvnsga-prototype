15ef14dd8668542df49f57f9de8311d20c598998
Oksana Shadura <oksana.shadura@cern.ch>
2016-06-28 23:47:52 +0200

    Adding additional constraints

--- libga/inc/gaoperators/GASimpleMutation.h
+++ libga/inc/gaoperators/GASimpleMutation.h
@@ -24,11 +24,26 @@ class GASimpleMutation : public GAMutation<GASimpleMutation> {
 
 public:
   template <typename T> static void MutationImpl(T &in, double prob = -1) {
+    double y;
+    double lb = in[0].GetDownLimit();
+    double ub = in[0].GetUpLimit();
     if (prob == -1)
       prob = 1 / (double)in.size();
     for (unsigned int i = 0; i < in.size(); ++i) {
-      if (Random::GetInstance().RandomDouble() < prob)
-        in[i] = in[i].random();
+      if (Random::GetInstance().RandomDouble() < prob) {
+        y = in[i].random();
+        if (std::isinf(y)) {
+          in[j] = lb;
+        }
+        if (std::isnan(y)) {
+          in[j] = ub;
+        }
+        if (y < lb)
+          y = lb;
+        if (y > ub)
+          y = ub;
+        in[j] = y;
+      }
     }
   }
 };
