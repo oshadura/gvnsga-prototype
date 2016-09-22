98dc04fc0b5dcc5cbce96f38bb955b8bb1dac15e
oshadura <oksana.shadura@cern.ch>
2016-09-22 19:43:12 +0200

    Getting back to single-core game - fix - 2 - 1 - 1 -1

--- tests/algorithms/GeantV.cxx
+++ tests/algorithms/GeantV.cxx
@@ -16,7 +16,7 @@ public:
 TEST_F(Nsga2, SolvingGeantVProblem) {
   geantvmoop::RunGeantV runc;
   geantvmoop::GANSGA2<geantvmoop::RunGeantV> nsga(runc);
-  nsga.fPopulationSize = 1;
+  nsga.fPopulationSize = 10;
   nsga.SolvePF();
 }
 #endif
