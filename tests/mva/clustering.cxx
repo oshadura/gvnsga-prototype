534b49fab2bc447101d715a7cb9c22f285c94758
oshadura <oksana.shadura@cern.ch>
2016-09-22 11:45:54 +0200

    Small modos to reduce allocations and concurrency testing

--- tests/mva/clustering.cxx
+++ tests/mva/clustering.cxx
@@ -17,7 +17,8 @@ class Clustering : public GATest {
 public:
   Spectral spcl;
   geantvmoop::DTLZ1 dtlz1;
-  geantvmoop::Population<geantvmoop::DTLZ1> pop{5};
+  // Reducing useless concurrency evaluations
+  //geantvmoop::Population<geantvmoop::DTLZ1> pop{5};
 };
 
 TEST_F(Clustering, SpectralClustering) {
